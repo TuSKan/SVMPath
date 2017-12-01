d558fbd9b0f234fe1c36f78a12c47f92a64274e7
Rener Castro <renercastro@hotmail.com>
2017-12-01 01:16:46 -0200

    Kernel Class

--- svm.h
+++ svm.h
@@ -1,104 +1,69 @@
-#ifndef _LIBSVM_H
-#define _LIBSVM_H
+#include <armadillo>
+#include "LRUCache.hpp"
 
-#define LIBSVM_VERSION 322
+using namespace std;
+using namespace arma;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 
-extern int libsvm_version;
+class Kernel {
+protected:
+	class KFun {
+	public:
+		// Constructor
+		KFun() {};
+		~KFun() {};
+		// Operator
+		virtual double operator() (const vec &Xi, const vec &Xj) {};
+		// Members
+	protected:
+		string name;
+		double param;
+	}; // KFun
 
-struct svm_node
-{
-	int index;
-	double value;
-};
+	// Linear Kernel
+	class KLinear : public KFun {
+	public:
+		KLinear() { name = "Linear"; }
+		double operator() (const vec &Xi, const vec &Xj) { 
+			return dot(Xi, Xj); 
+		}
+	}; // KLinear
 
-struct svm_problem
-{
-	int l;
-	double *y;
-	struct svm_node **x;
-};
+	// Gaussian Kernel
+	class KRBF : public KFun {
+	public:
+		KRBF(double gamma = NULL) { param = gamma; name = "RBF"; }
+		double operator() (const vec &Xi, const vec &Xj) { 
+			return exp(-param*(dot(Xi,Xi) + dot(Xj, Xj) - 2 * dot(Xi, Xj)));
+		}
+	}; // KRBF
 
-enum { C_SVC, NU_SVC, ONE_CLASS, EPSILON_SVR, NU_SVR };	/* svm_type */
-enum { LINEAR, POLY, RBF, SIGMOID, PRECOMPUTED }; /* kernel_type */
+	// Polynomial Kernel
+	class KPolynomial : public KFun {
+	private:
+		unsigned int degree;
+		double coef0;
+	public:
+		KPolynomial(double gamma = NULL, unsigned int degree = NULL, double coef0 = NULL) : degree(degree), coef0(coef0) { param = gamma; name = "Polynomial"; }
+		double operator() (const vec &Xi, const vec &Xj) {
+			return pow(param * dot(Xi, Xj) + coef0, degree);
+		}
+	};
 
-struct svm_parameter
-{
-	int svm_type;
-	int kernel_type;
-	int degree;	/* for poly */
-	double gamma;	/* for poly/rbf/sigmoid */
-	double coef0;	/* for poly/sigmoid */
+	// Sigmoid kernel
+	class KSigmoid : public KFun {
+	private:
+		double coef0;
+	public:
+		KSigmoid(double gamma = NULL, double coef0 = NULL) : coef0(coef0) { param = gamma; name = "Sigmoid"; }
+		double operator() (const vec &Xi, const vec &Xj) {
+			return tanh(param * dot(Xi, Xj) + coef0);
+		}
+	};
 
-	/* these are for training only */
-	double cache_size; /* in MB */
-	double eps;	/* stopping criteria */
-	double C;	/* for C_SVC, EPSILON_SVR and NU_SVR */
-	int nr_weight;		/* for C_SVC */
-	int *weight_label;	/* for C_SVC */
-	double* weight;		/* for C_SVC */
-	double nu;	/* for NU_SVC, ONE_CLASS, and NU_SVR */
-	double p;	/* for EPSILON_SVR */
-	int shrinking;	/* use the shrinking heuristics */
-	int probability; /* do probability estimates */
-};
+public: 
+	Kernel(mat * const * X, vec const * Y, const KFun &K);
+	~Kernel();
+}; // Kernel
 
-//
-// svm_model
-// 
-struct svm_model
-{
-	struct svm_parameter param;	/* parameter */
-	int nr_class;		/* number of classes, = 2 in regression/one class svm */
-	int l;			/* total #SV */
-	struct svm_node **SV;		/* SVs (SV[l]) */
-	double **sv_coef;	/* coefficients for SVs in decision functions (sv_coef[k-1][l]) */
-	double *rho;		/* constants in decision functions (rho[k*(k-1)/2]) */
-	double *probA;		/* pariwise probability information */
-	double *probB;
-	int *sv_indices;        /* sv_indices[0,...,nSV-1] are values in [1,...,num_traning_data] to indicate SVs in the training set */
 
-	/* for classification only */
-
-	int *label;		/* label of each class (label[k]) */
-	int *nSV;		/* number of SVs for each class (nSV[k]) */
-				/* nSV[0] + nSV[1] + ... + nSV[k-1] = l */
-	/* XXX */
-	int free_sv;		/* 1 if svm_model is created by svm_load_model*/
-				/* 0 if svm_model is created by svm_train */
-};
-
-struct svm_model *svm_train(const struct svm_problem *prob, const struct svm_parameter *param);
-void svm_cross_validation(const struct svm_problem *prob, const struct svm_parameter *param, int nr_fold, double *target);
-
-int svm_save_model(const char *model_file_name, const struct svm_model *model);
-struct svm_model *svm_load_model(const char *model_file_name);
-
-int svm_get_svm_type(const struct svm_model *model);
-int svm_get_nr_class(const struct svm_model *model);
-void svm_get_labels(const struct svm_model *model, int *label);
-void svm_get_sv_indices(const struct svm_model *model, int *sv_indices);
-int svm_get_nr_sv(const struct svm_model *model);
-double svm_get_svr_probability(const struct svm_model *model);
-
-double svm_predict_values(const struct svm_model *model, const struct svm_node *x, double* dec_values);
-double svm_predict(const struct svm_model *model, const struct svm_node *x);
-double svm_predict_probability(const struct svm_model *model, const struct svm_node *x, double* prob_estimates);
-
-void svm_free_model_content(struct svm_model *model_ptr);
-void svm_free_and_destroy_model(struct svm_model **model_ptr_ptr);
-void svm_destroy_param(struct svm_parameter *param);
-
-const char *svm_check_parameter(const struct svm_problem *prob, const struct svm_parameter *param);
-int svm_check_probability_model(const struct svm_model *model);
-
-void svm_set_print_string_function(void (*print_func)(const char *));
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif /* _LIBSVM_H */
