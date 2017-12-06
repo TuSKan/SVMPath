255314d6085c8bd456b812960d7ec244c212a6f2
Rener Castro <renercastro@hotmail.com>
2017-12-06 05:48:10 -0200

    dot KMatrix

--- svm.cpp
+++ svm.cpp
@@ -1,66 +1,74 @@
 #include <iostream>
 #include "svm.h"
 
-//int main(int argc, char **argv)
-//{
-//	mat X = randu<mat>(5, 5);
-//	X.print("X: ");
-//
-//	KRBF K(&X, 0.1);
-//
-//	cout << endl;
-//	cout << K(0, 0) << "\t" << K(0, 1) << "\t" << K(0, 2) << "\t" << K(0, 3) << "\t" << K(0, 4) << "\t" << endl;
-//	cout << K(1, 0) << "\t" << K(1, 1) << "\t" << K(1, 2) << "\t" << K(1, 3) << "\t" << K(1, 4) << "\t" << endl;
-//	cout << K(2, 0) << "\t" << K(2, 1) << "\t" << K(2, 2) << "\t" << K(2, 3) << "\t" << K(2, 4) << "\t" << endl;
-//	cout << K(3, 0) << "\t" << K(3, 1) << "\t" << K(3, 2) << "\t" << K(3, 3) << "\t" << K(3, 4) << "\t" << endl;
-//	cout << K(4, 0) << "\t" << K(4, 1) << "\t" << K(4, 2) << "\t" << K(4, 3) << "\t" << K(4, 4) << "\t" << endl;
-//	cout << endl;
-//
-//	cout << "Cache size: " << K.Cache()->size() << endl;
-//
-//	system("pause");
-//	return 0;
-//}
-//
 int main(int argc, char **argv)
 {
-	mat X = randu<mat>(1000000, 500);
-	mat A(X.n_cols, X.n_cols);
+	uword n = 100000;
+	uword m = 400;
+	mat X = randu<mat>(n, m);
+	mat y = randu<vec>(m);
+	mat A(n, m);
 
 	KRBF K(&X, 0.1);
 
 	auto start = std::chrono::system_clock::now();
-#pragma omp parallel for //collapse(2)
-	for (int i = 0; i < K.ncols; i++) {
-		for (int j = 0; j < K.ncols; j++) {
-			A(i,j) = K(i, j);
-		}
-	}
+	dot(y, K);
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed = end - start;
 	cout << "Elapse: " << elapsed.count() << " seconds" << endl;
 
 	auto start2 = std::chrono::system_clock::now();
-#pragma omp parallel for //collapse(2)
-	for (int i = 0; i < K.ncols; i++) {
-		for (int j = 0; j < K.ncols; j++) {
-			auto tmp = A(i, j);
-		}
-	}
+	dot(y, K);
 	auto end2 = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed2 = end2 - start2;
 	cout << "Elapse: " << elapsed2.count() << " seconds" << endl;
 
-	auto start3 = std::chrono::system_clock::now();
-#pragma omp parallel for //collapse(2)
-	for (int i = 0; i < K.ncols; i++) {
-		for (int j = 0; j < K.ncols; j++) {
-			auto tmp = K(i, j);
+//#pragma omp parallel for
+	for (int i = 0; i < K.dim; i++) {
+		for (int j = 0; j < K.dim; j++) {
+			A(i,j) = K(i, j);
 		}
 	}
+
+	auto start3 = std::chrono::system_clock::now();
+	dot(y, A.t());
 	auto end3 = std::chrono::system_clock::now();
 	std::chrono::duration<double> elapsed3 = end3 - start3;
 	cout << "Elapse: " << elapsed3.count() << " seconds" << endl;
+
+
+//	auto start = std::chrono::system_clock::now();
+//#pragma omp parallel for //collapse(2)
+//	for (int i = 0; i < K.dim; i++) {
+//		for (int j = 0; j < K.dim; j++) {
+//			A(i,j) = K(i, j);
+//		}
+//	}
+//	auto end = std::chrono::system_clock::now();
+//	std::chrono::duration<double> elapsed = end - start;
+//	cout << "Elapse: " << elapsed.count() << " seconds" << endl;
+//
+//	auto start2 = std::chrono::system_clock::now();
+//#pragma omp parallel for //collapse(2)
+//	for (int i = 0; i < K.dim; i++) {
+//		for (int j = 0; j < K.dim; j++) {
+//			auto tmp = A(i, j);
+//		}
+//	}
+//	auto end2 = std::chrono::system_clock::now();
+//	std::chrono::duration<double> elapsed2 = end2 - start2;
+//	cout << "Elapse: " << elapsed2.count() << " seconds" << endl;
+//
+//	auto start3 = std::chrono::system_clock::now();
+//#pragma omp parallel for //collapse(2)
+//	for (int i = 0; i < K.dim; i++) {
+//		for (int j = 0; j < K.dim; j++) {
+//			auto tmp = K(i, j);
+//		}
+//	}
+//	auto end3 = std::chrono::system_clock::now();
+//	std::chrono::duration<double> elapsed3 = end3 - start3;
+//	cout << "Elapse: " << elapsed3.count() << " seconds" << endl;
 
 	system("pause");
 	return 0;
--- svm.h
+++ svm.h
@@ -1,88 +1,142 @@
 #include <armadillo>
-#include "LRUCache.hpp"
 
 using namespace std;
 using namespace arma;
 
-class KFun {
+const uvec sequence(const uword& begin, const uword& end) {
+	uvec r(end - begin + 1);
+	for (uword i = begin; i < end + 1; ++i)
+		r[i - begin] = i;
+	return r;
+}
+
+
+class KMatrix {
 public:
 	// Constructor
-	KFun(const mat * X = NULL, const string name = NULL, const double param = NULL) : X(X), name(name), param(param), ncols(X->n_cols) {};
-	~KFun() {};
+	KMatrix(const mat * X = NULL, const string name = NULL, const double param = NULL) 
+		: X(X), name(name), param(param), dim(X->n_cols), size((X->n_cols + X->n_cols*X->n_cols) / 2), count(0) {
+		MTri.resize(size);
+		MTri.fill(numeric_limits<double>::quiet_NaN());
+	};
+	~KMatrix() { };
 	// Operator
-	virtual double operator() (const unsigned int i, const unsigned int j) {};
+	virtual double operator() (const uword i, const uword j) {};
 	// Accessors
-	const lru::Cache<unsigned int, double, mutex>* Cache() { return &KCache; }
+	const bool isFilled() const { return count == size; }
+	const vec& getMtri() const { return MTri; }
 	// Members
 protected:
 	string name;
 	double param;
-	lru::Cache<unsigned int, double, mutex> KCache;
+	vec MTri;
+	const uword size;
 	const mat * X;
+	uword count;
 public:
-	const unsigned int ncols;
+	const uword dim;
 }; // KFun
 
+const vec dot(const vec& v, KMatrix& K) {
+	arma_assert_same_size(v.n_rows, v.n_cols, K.dim, 1, string("dot KMatrix").c_str());
+	vec res(K.dim);
+	if (K.isFilled()) {
+		const vec& MTri = K.getMtri();
+//#pragma omp parallel for
+		for (uword j = 0; j < K.dim; ++j) {
+			uvec ID = zeros<uvec>(K.dim);
+			for (uword i = 0; i < j; ++i) ID(i) = j + i * K.dim - accu(sequence(0, i));
+			ID(sequence(j, K.dim - 1)) = sequence(j + j * K.dim - accu(sequence(0, j)), K.dim - 1 + j * K.dim - accu(sequence(0, j)));
+			res(j) = dot(v, MTri.elem(ID));
+		}
+	}
+	else {		
+//#pragma omp parallel for
+		for (uword j = 0; j < K.dim; ++j) {
+			double tmp(0);
+			for (uword i = 0; i < K.dim; ++i) {
+				tmp += v[i] * K(i, j);
+			}
+			res(j) = tmp;
+		}
+	}
+	return res;
+}
+
+const vec dot(KMatrix& K, const vec& v) {  return dot(v, K); }
+
    // Linear Kernel
-class KLinear : public KFun {
+class KLinear : public KMatrix {
 public:
-	KLinear(const mat * X) : KFun(X, "Linear") {}
-	double operator() (const unsigned int i, const unsigned int j) {
-		double res;
-		unsigned int IJ = (i < j ? i : j) + (i > j ? i : j) * ncols;
-		if (!KCache.tryGet(IJ, res)) {
+	KLinear(const mat * X) : KMatrix(X, "Linear") {}
+	double operator() (const uword i, const uword j) {
+		uword IJ;
+		if (i < j) IJ = j + i * dim - accu(sequence(0, i));
+		else IJ = i + j * dim - accu(sequence(0, j));
+		double res = MTri[IJ];
+		if (isnan(res)) {
 			res = dot(X->col(i), X->col(j));
-			KCache.insert(IJ, res);
+			MTri[IJ] = res;
+			++count;
 		}
 		return res;
 	}
 }; // KLinear
 
-   // Gaussian Kernel
-class KRBF : public KFun {
+   // RBF Kernel
+class KRBF : public KMatrix {
 public:
-	KRBF(const mat * X, double gamma) : KFun(X, "RBF", gamma) {}
-	double operator() (const unsigned int i, const unsigned int j) {
-		double res;
-		unsigned int IJ = (i < j ? i : j) + (i > j ? i : j) * ncols;
-		if (!KCache.tryGet(IJ, res)) {
+	KRBF(const mat * X, double gamma) : KMatrix(X, "RBF", gamma) {}
+	double operator() (const uword i, const uword j) {
+		uword IJ;
+		if (i < j) IJ = j + i * dim - accu(sequence(0,i));
+		else IJ = i + j * dim - accu(sequence(0, j));
+		double res = MTri[IJ];
+		if (isnan(res)) {
 			res = exp(-param*(dot(X->col(i), X->col(i)) + dot(X->col(j), X->col(j)) - 2 * dot(X->col(i), X->col(j))));
-			KCache.insert(IJ, res);
+			MTri[IJ] = res;
+			++count;
 		}
 		return res;
 	}
 }; // KRBF
 
    // Polynomial Kernel
-class KPolynomial : public KFun {
+class KPolynomial : public KMatrix {
 private:
-	unsigned int degree;
+	uword degree;
 	double coef0;
 public:
-	KPolynomial(const double gamma, const unsigned int degree, const double coef0 = NULL) : KFun(X, "Polynomial", gamma), degree(degree), coef0(coef0) {}
-	double operator() (const unsigned int i, const unsigned int j) {
-		double res;
-		unsigned int IJ = (i < j ? i : j) + (i > j ? i : j) * ncols;
-		if (!KCache.tryGet(IJ, res)) {
+	KPolynomial(const double gamma, const uword degree, const double coef0 = NULL) : KMatrix(X, "Polynomial", gamma), degree(degree), coef0(coef0) {}
+	double operator() (const uword i, const uword j) {
+		uword IJ;
+		if (i < j) IJ = j + i * dim - accu(sequence(0, i));
+		else IJ = i + j * dim - accu(sequence(0, i));
+		double res = MTri[IJ];
+		if (isnan(res)) {
 			res = pow(param * dot(X->col(i), X->col(j)) + coef0, degree);
-			KCache.insert(IJ, res);
+			MTri[IJ] = res;
+			++count;
 		}
 		return res;
 	}
 };
 
 // Sigmoid kernel
-class KSigmoid : public KFun {
+class KSigmoid : public KMatrix {
 private:
 	double coef0;
 public:
 	KSigmoid(double gamma, double coef0 = NULL) : coef0(coef0) { param = gamma; name = "Sigmoid"; }
-	double operator() (const unsigned int i, const unsigned int j) {
-		double res;
-		unsigned int IJ = (i < j ? i : j) + (i > j ? i : j) * ncols;
-		if (!KCache.tryGet(IJ, res)) {
+	double operator() (const uword i, const uword j) {
+		uword IJ;
+		if (i < j) IJ = j + i * dim - accu(sequence(0, i));
+		else IJ = i + j * dim - accu(sequence(0, i));
+		double res = MTri[IJ];
+		if (isnan(res)) {
 			res = tanh(param * dot(X->col(i), X->col(j)) + coef0);
-			KCache.insert(IJ, res);
+			MTri[IJ] = res;
+			++count;
 		}
 		return res;
 	}
